aa72b8bda66aee28014158088e98a7fbffbe99b1
zjoye <zjoye@foxmail.com>
2024-05-05 22:19:43 +0800

    feat:1.树-水平遍历-队列确保先后顺序

---
+++ src/main/java/tree/LevelOrDerTraversalTree.java
@@ -0,0 +1,37 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://leetcode.cn/problems/binary-tree-level-order-traversal/
+ */
+public class LevelOrDerTraversalTree {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // 队列用于确保层序
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if(root==null){
+            return new ArrayList<>();
+        }
+        // 确保根节点先为首层的层序
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> tmpList = new ArrayList<>();
+            for (int i = 0; i < queue.size() - 1; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                tmpList.add(node.val);
+            }
+            result.add(tmpList);
+        }
+        return result;
+    }
+}
