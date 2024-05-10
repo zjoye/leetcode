a8b532bb8e059c839305dd1dcb4914b0801179b7
zjoye <zjoye@foxmail.com>
2024-05-10 21:06:51 +0800

    feat:1.树-水平平均值

---
+++ src/main/java/tree/AverageOfLevel.java
@@ -0,0 +1,45 @@
+package tree;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://leetcode.cn/problems/average-of-levels-in-binary-tree/
+ */
+public class AverageOfLevel {
+    public static List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            //double average = new BigDecimal(sum).divide(new BigDecimal(size), 5, RoundingMode.HALF_UP).doubleValue();
+
+            result.add(sum / size);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        averageOfLevels(root);
+    }
+}
