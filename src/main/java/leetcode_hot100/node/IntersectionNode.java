32daa4eb236249b191e9a9e6718f9d44fd09bc32
zjoye <zjoye@foxmail.com>
2025-04-06 16:41:33 +0800

    leetcode_hot100:1.矩阵搜素、相交链表

---
+++ src/main/java/leetcode_hot100/node/IntersectionNode.java
@@ -0,0 +1,15 @@
+package leetcode_hot100.node;
+
+import linkList.ListNode;
+
+public class IntersectionNode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode A = headA;
+        ListNode B = headB;
+        while (A != B) {
+            A = A != null ? A.next : headB;
+            B = B != null ? B.next : headA;
+        }
+        return A;
+    }
+}
